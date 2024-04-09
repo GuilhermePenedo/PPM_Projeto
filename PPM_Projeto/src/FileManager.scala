d78cde2e25f7ddec7965b7e2fec2ab870874a38b
pcp2003 <124909431+pcp2003@users.noreply.github.com>
2024-04-09 19:08:32 +0100

    PPM_Projeto_Parte1
    Commit agora com o ficheiro txt

---
+++ PPM_Projeto/src/FileManager.scala
@@ -0,0 +1,25 @@
+import Main.{Coord2D, HiddenWord}
+
+import scala.io.Source
+
+object FileManager {
+
+  def lerPalavrasEscondidas(caminho: String): List[HiddenWord] = {
+
+    Source.fromFile(caminho).getLines().map { linha =>
+      val Array(texto, paresTexto) = linha.split(",List", 2)
+      val pares = paresTexto.drop(2).dropRight(2).split("\\),\\(").toList.map {
+        par =>
+          val Array(x, y) = par.split(",", 2).map(_.toInt)
+          (x, y)
+      }
+      (texto, pares)
+    }.toList
+  }
+
+  def NumberOfWordsToFind(caminho: String): Int = {
+    Source.fromFile(caminho).getLines().size
+  }
+
+
+}
