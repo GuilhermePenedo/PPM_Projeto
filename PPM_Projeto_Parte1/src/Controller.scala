3d3fbecad68709f803960ef99db850f66f37be43
Guilherme Penedo <guipedroborges@gmail.com>
2024-05-08 10:26:00 +0100

    java fx

---
+++ PPM_Projeto_Parte1/src/Controller.scala
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, TextField}
+class Controller {
+  @FXML
+  private var button1: Button = _
+  @FXML
+  private var textField1: TextField = _
+  def onButton1Clicked(): Unit = {
+    println(textField1.getText())
+  }
+}
