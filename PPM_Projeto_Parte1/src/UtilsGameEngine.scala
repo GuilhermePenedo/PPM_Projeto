f932555f72dcdc7748ae9137bc773bd4bf0da2cb
Guilherme Penedo <guipedroborges@gmail.com>
2024-05-12 17:17:58 +0100

    fix play e adicionei uma palabra
    o play tinha um bug na procura do caminho e adicionei MARGARIDA

--- PPM_Projeto_Parte1/src/UtilsGameEngine.scala
+++ PPM_Projeto_Parte1/src/UtilsGameEngine.scala
@@ -65,30 +65,29 @@ object UtilsGameEngine {
   }
 
   def play(wordInput : String, board: Board, startPos: Coord2D, dir: Direction): Boolean = {
-      countPaths(wordInput : String, board: Board, startPos: Coord2D, dir: Direction) > 0
+      countPaths(wordInput,board,startPos,dir, Nil) > 0
   }
 
   def correctGuess(word: String, initialCoord:Coord2D, initialDirection:Direction, gameState:GameState):Boolean = {
     play(word, gameState.board._1, initialCoord, initialDirection) && inList(word, gameState.wordsToFind map (x => x._1))
   }
 
-  def countPaths(wordInput : String, board: Board, startPos: Coord2D, dir: Direction): Int = {
+  def countPaths(wordInput : String, board: Board, startPos: Coord2D, dir: Direction, searchedPos:List[Coord2D]): Int = {
     def isValidPos(pos: Coord2D, board:Board): Boolean = {
       val (i,j) = pos
       i >= 0 && i < board.length && j >= 0 && j < getItem(board, i).length
     }
-
     val (i1,j1) = startPos
     val (i2, j2) = Direction.nextPos(dir, startPos)
     val wordList = wordInput.toList
     val isCorrectChar = getItem(getItem(board, i1), j1) == getItem(wordList, 0)
-    val directionList = Direction.values.toList filter (x => x != Direction.getOpposite(dir) &&  x != INVALID)
-    if(isValidPos((i2, j2), board) && isCorrectChar && getItem(getItem(board, i2), j2) == getItem(wordList, 1)) {
-      searchDirections(wordInput.tail.toList, board, (i2, j2), directionList)
+    val directionList = Direction.values.toList filter (x => x != INVALID)
+    if(isValidPos((i2, j2), board) && isCorrectChar && getItem(getItem(board, i2), j2) == getItem(wordList, 1) && !inList((i2,j2),searchedPos)) {
+      searchDirections(wordInput.tail.toList, board, (i2, j2), directionList, searchedPos)
     }else 0
   }
 
-  def searchDirections(word: List[Char], board: Board, startPos: Coord2D, directions: List[Direction]): Int = {
+  def searchDirections(word: List[Char], board: Board, startPos: Coord2D, directions: List[Direction], searchedPos:List[Coord2D]): Int = {
   val (i1, j1) = startPos
   val isCorrectChar = getItem (getItem (board, i1), j1) == getItem (word, 0)
   if (word.length == 1) {
@@ -97,7 +96,7 @@ object UtilsGameEngine {
   }else {
     directions match {
         case Nil => 0
-        case dir :: tail => countPaths(listToString(word), board, startPos, dir) + searchDirections(word, board, startPos, tail)
+        case dir :: tail => countPaths(listToString(word), board, startPos, dir, searchedPos:+startPos) + searchDirections(word, board, startPos, tail, searchedPos:+startPos)
       }
     }
   }
@@ -107,15 +106,13 @@ object UtilsGameEngine {
     def countOccurrenceInBoard(board: Board, word:List[Char], directions: List[Direction]): Int = {
       def resultRow( current : (Char,Coord2D), res:Int): Int = {
         if(current._1 == word.head) {
-          searchDirections(word, board, current._2,directions) + res
+          searchDirections(word, board, current._2,directions, Nil) + res
         }else {
           res
         }
       }
       iterateMatrix(board, (a:Int, b:Int) => (a+b) , resultRow, 0, 0)
-
     }
-
     val directions = Direction.values.toList filter (x => x != INVALID)
     val correctNrWord = wordsToFind map (x => if(isPalindrome(x.toList)) 2 else 1)
     val wordsOccurrences = wordsToFind map (x => (countOccurrenceInBoard(board, x.toList, directions)))
